35a1e0eda83cc0ed8bc06f27baa0a0594b15f490
Emerson Demetrio <emer.demetrio@gmail.com>
2017-09-02 12:09:31 -0300

    Untranslated Tracks

--- expose_steps/step3_extract_mbid.py
+++ expose_steps/step3_extract_mbid.py
@@ -11,93 +11,165 @@ from util.httpy import Httpy
 from util.tracks_db import TracksDb
 from crawler.last_fm import LastFm
 
-def url_encode(track):
-    """ url encode """
-    return urllib.parse.urlencode({
-        "artist": utils.sanitize_string(track["artist"].replace("\uf022", ""), [], force=True),
-        "track": re.sub(r"\w*\d\w*", "", track["track"]).strip()
-    })
+def check_result(result):
+    """Check request result"""
 
-def parse_level_1(track):
-    """ parse lvl 1 """
-    return {
-        "artist": track["artist"],
-        "track": track["name"],
-        "mbid": track["mbid"]
-    }
+    if "results" in result:
+        return True
+    return False
 
-def run_tests(json_file, last_fm, output_file, limit=False, request_limit=300000):
+def get_mbid(result):
+    """Check request result"""
+
+    result = result["results"]
+    if int(result["opensearch:totalResults"]) > 0:
+        if result["trackmatches"]["track"][0]["mbid"] != "":
+            mbid = result["trackmatches"]["track"][0]["mbid"]
+            return mbid
+    return ""
+
+def request_attempt(track, req_url, last_fm, tdb):
+    """do request and update"""
+
+    incoming_response = last_fm.search_track(req_url)
+
+    if check_result(incoming_response):
+        mbid = get_mbid(incoming_response)
+        if mbid:
+            tdb.update_mbid(track, mbid)
+            return True
+    return False
+
+def run_tests(json_file, last_fm, limit=False, untranslated_only=False, request_limit=300000):
     """ running tests """
-    tdb = TracksDb()
 
+    tdb = TracksDb()
     all_tracks = utils.read_json_file(json_file)
 
     if limit:
         all_tracks = all_tracks[:request_limit]
 
-    responses = []
-    for pre_track in all_tracks:
-        track = json.loads(pre_track)
-        req_url = url_encode(track)
-        incoming_response = last_fm.search_track(req_url)
+    if not untranslated_only:
+        for pre_track in all_tracks:
+            track = json.loads(pre_track)
 
-        tmp_rp = incoming_response["results"]
+            parsed_track_name = re.sub(r"\w*\d\w*", "", track["track"]).strip()
+            parsed_track_name = re.sub(r'\d+', '', parsed_track_name)
 
-        if (int(tmp_rp["opensearch:totalResults"]) > 0
-                and tmp_rp["trackmatches"]["track"][0]["mbid"] == ""):
+            attempts = [
+                urllib.parse.urlencode({
+                    "artist": utils.sanitize_string(
+                        track["artist"].replace("\uf022", ""), [], force=True
+                    ),
+                    "track": track["track"].strip()
+                }),
+                urllib.parse.urlencode({
+                    "track": re.sub(r"\w*\d\w*", "", track["track"]).strip()
+                }),
+                urllib.parse.urlencode({
+                    "track": parsed_track_name
+                }),
+                urllib.parse.urlencode({
+                    "track": re.sub(r'\d+', '', track["track"])
+                }),
+                urllib.parse.urlencode({
+                    "artist": utils.sanitize_string(
+                        track["artist"].replace("\uf022", ""), [], force=True
+                    ),
+                    "track": re.sub(r'\d+', '', track["track"])
+                }),
+            ]
 
-            new_attempt_url = urllib.parse.urlencode({
+            for request_url in attempts:
+                print("Track: {}\nAttempt:{}\n\n".format(track['track'], request_url))
+                translated = request_attempt(track, request_url, last_fm, tdb)
+
+                if translated:
+                    break
+
+    proced_untranslated_tracks(tdb, last_fm)
+
+def proced_untranslated_tracks(tdb, last_fm):
+    """Untranslated Tracks in DB"""
+    conditions = [{
+        "field": "mbid",
+        "value": "IS NULL"
+    }]
+
+    tracks = tdb.get_tracks("id, artist, name", conditions)
+
+    for track in tracks:
+        track["track"] = track["name"]
+        print("Track: ", track)
+        parsed_track_name = re.sub(r"\w*\d\w*", "", track["track"]).strip()
+        parsed_track_name = re.sub(r'\d+', '', parsed_track_name)
+
+        """
+        Chage this!
+        """
+        attempts = [
+            urllib.parse.urlencode({
+                "artist": utils.sanitize_string(
+                    track["artist"].replace("\uf022", ""), [], force=True
+                ),
+                "track": track["track"].strip()
+            }),
+            urllib.parse.urlencode({
                 "track": re.sub(r"\w*\d\w*", "", track["track"]).strip()
-            })
+            }),
+            urllib.parse.urlencode({
+                "track": parsed_track_name
+            }),
+            urllib.parse.urlencode({
+                "track": re.sub(r'\d+', '', track["track"])
+            }),
+            urllib.parse.urlencode({
+                "artist": utils.sanitize_string(
+                    track["artist"].replace("\uf022", ""), [], force=True
+                ),
+                "track": re.sub(r'\d+', '', track["track"])
+            }),
+        ]
 
-            incoming_response = last_fm.search_track(new_attempt_url)
 
-        if (int(tmp_rp["opensearch:totalResults"]) > 0
-                and tmp_rp["trackmatches"]["track"][0]["mbid"] != ""):
-                tdb.update_mbid(track, tmp_rp["trackmatches"]["track"][0]["mbid"])
+        for request_url in attempts:
+            print("Track: {}\nAttempt:{}\n\n".format(track['track'], request_url))
+            translated = request_attempt(track, request_url, last_fm, tdb)
 
-        responses.append(incoming_response)
-
-    possible_mbid_tracks = []
-
-    for rep in responses:
-        resp = rep["results"]
-
-        if int(resp["opensearch:totalResults"]) > 0:
-            possible_mbid_tracks.append(
-                parse_level_1(
-                    resp["trackmatches"]["track"][0]
-                )
-            )
-
-    utils.write_json_file(output_file, possible_mbid_tracks)
-
+            if translated:
+                break
 def main():
     """ main """
 
     desc = "API LVL 1"
     parser = argparse.ArgumentParser(description=desc)
 
-    print("\n[ TCC - API LVL 1 ]\n%s\n" % desc)
+    print("\n[ TCC - API XT MBID 1 ]\n%s\n" % desc)
     parser.add_argument('-k', '--api_key', required=True)
     parser.add_argument('-f', '--input_folder', default=utils.get_valid_folder_path("results"))
     parser.add_argument('-i', '--input_file', default="output")
-    parser.add_argument('-o', '--output_file', default="level1")
     parser.add_argument('-lp', '--limit_population', default="no")
+    parser.add_argument('-uo', '--untranslated_only', default="no")
     parser.add_argument('-tl', '--tracks_limit', default=300000)
     parsed = parser.parse_args()
 
     json_file = os.path.join(parsed.input_folder, parsed.input_file + ".json")
-    output_file = os.path.join(parsed.input_folder, parsed.output_file + ".json")
 
     tracks_limit = int(parsed.tracks_limit)
     limit_population = False if parsed.limit_population == "no" else True
+    untranslated_only = False if parsed.untranslated_only == "no" else True
 
     last_fm_key = parsed.api_key
     http_py = Httpy()
     last_fm = LastFm(last_fm_key, "http", "ws.audioscrobbler.com/2.0/", "json", http_py)
 
-    run_tests(json_file, last_fm, output_file, limit=limit_population, request_limit=tracks_limit)
+    run_tests(
+        json_file,
+        last_fm,
+        limit=limit_population,
+        untranslated_only=untranslated_only,
+        request_limit=tracks_limit
+    )
 
 
 if __name__ == "__main__":
--- util/tracks_db.py
+++ util/tracks_db.py
@@ -73,10 +73,12 @@ class TracksDb:
         try:
             connection = self.get_connection()
             with connection.cursor() as cursor:
-                sql = "UPDATE `track` SET `{}` = '{}' WHERE `track`.id = {}".format(
-                    field, val, track_id,
+                sql = "UPDATE `track` SET {} = '{}' WHERE `track`.id = {}".format(
+                    field, val, track_id
                 )
-                cursor.execute(sql)
+                #print(sql)
+                cursor.execute(sql, ())
+                connection.commit()
         finally:
             connection.close()
 
@@ -87,24 +89,61 @@ class TracksDb:
             connection = self.get_connection()
             with connection.cursor() as cursor:
                 sql = "SELECT id FROM `track` WHERE name = (%s)"
-                cursor.execute(sql, (track["track"]))
+                #print(sql)
+                cursor.execute(sql, (track["track"],))
 
                 try:
-                    self.update_track(cursor.fetchone()['id'], 'mbid', mbid)
+                    result = cursor.fetchone()
+                    if result != None:
+                        track_id = result['id']
+                        print("Track: {}\nMBID: {}\n".format(track_id, mbid))
+                        self.update_track(track_id, 'mbid', mbid)
                 except AttributeError:
                     utils.show_message("Unable to update MIBD for %s" % track['track'], 1)
 
         finally:
             connection.close()
 
-    def get_tracks(self, limit=300000):
+    def create_select_sql(self, fields, conditions, limit):
+        """Create SELECT sql"""
+
+        sql = "SELECT {} FROM `track` WHERE ".format(fields)
+        where_condition = ""
+        count = 0
+
+        for condition in conditions:
+            if count == 0:
+                where_condition = "{} {}".format(
+                    condition['field'],
+                    condition['value']
+                )
+            else:
+                where_condition = "{} AND {} {}".format(
+                    where_condition,
+                    condition['field'],
+                    condition['value']
+                )
+
+            count = count + 1
+
+        sql = "{} {} LIMIT {}".format(sql, where_condition, limit)
+        return sql
+
+    def get_tracks(self, fields="*", conditions=None, limit=300000):
         """Get All Tracks"""
+
+        if conditions:
+            sql = self.create_select_sql(fields, conditions, limit)
+        else:
+            sql = "SELECT {} FROM `track` LIMIT {}".format(fields, limit)
+
+        print(sql)
 
         try:
             connection = self.get_connection()
             with connection.cursor() as cursor:
-                sql = "SELECT * FROM `track` LIMIT (%s)"
-                cursor.execute(sql, (limit))
+                cursor.execute(sql)
                 return cursor.fetchall()
         finally:
             connection.close()
+
