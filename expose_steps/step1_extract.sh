425bc7b8aa970b163a7fa6f49ff6a74e8c07620f
Emerson Demetrio <emer.demetrio@gmail.com>
2017-09-05 01:07:13 -0300

    Creating init.sh file; removing unused code

--- expose_steps/step1_extract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=.
-
-echo "STEP 1 - Extract Tracks to csv/json file:"
-
-python3 file_analyst/track_extractor.py -i /media/emerson/Dados/music/
---
+++ init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PYTHONPATH=.
+
+LAST_FM_KEY="last-fm-key-xD"
+
+echo "STEP 1 - Extract Tracks to csv/json file:"
+
+python3 file_analyst/track_extractor.py -i /media/emerson/Dados/music/;
+
+echo "STEP 2 - Populate DB:"
+
+python3 expose_steps/step2_poupate_db.py;
+
+echo "STEP 3 - Update MBID:"
+
+python3 expose_steps/step3_extract_mbid.py -k $LAST_FM_KEY;
+
+echo "STEP 4 - Update TAGS:"
+
+python3 expose_steps/tag_crawler.py -k $LAST_FM_KEY;
+
+echo "STEP 4 - Create Spectros:"
+
+python3 expose_steps/image_creator.py -n 1000 -workers_number=20
+
+echo "STEP 5 - Training:"
+echo "NOT YET IMPLEMENTED"
+
+echo "STEP 6 - Analysis:"
+echo "NOT YET IMPLEMENTED"
+
+echo "STEP 7 - Test with new File:"
+echo "NOT YET IMPLEMENTED"
