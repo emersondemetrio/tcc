66951ba7c01b2a1aba440a888b10f8251d326e31
Emerson Demetrio <emerson.demetrio@scytech.com.br>
2018-03-09 13:04:39 -0300

    Json To CSV Working

--- jsonToCSV/index.js
+++ jsonToCSV/index.js
@@ -1,35 +1,21 @@
-const process = require('process');
+// http://kauegimenes.github.io/jsonexport/
+
 const fs = require('fs');
+const process = require('process');
 const resolve = require('path').resolve
-const Json2csvTransform = require('json2csv').Transform;
+var jsonexport = require('jsonexport');
 
 const folderPath = process.argv[2];
+
+// npm start /home/emerson/projects/tcc/input # for example
 
 console.log("Reading: ", folderPath);
 
 const parseJsonFile = (inputPath, outputPath) => {
-
-	const fields = ['field1', 'field2', 'field3'];
-	const opts = {
-		fields
-	};
-
-	const transformOpts = {
-		highWaterMark: 16384,
-		encoding: 'utf-8'
-	};
-
 	const input = fs.createReadStream(inputPath, { encoding: 'utf8' });
 	const output = fs.createWriteStream(outputPath, { encoding: 'utf8' });
-	const json2csv = new Json2csvTransform({}, transformOpts);
 
-	const processor = input.pipe(json2csv).pipe(output);
-
-	// You can also listen for events on the conversion and see how the header or the lines are coming out.
-	json2csv
-		.on('header', header => console.log(header))
-		.on('line', line => console.log(line))
-		.on('error', err => console.log(err));
+	input.pipe(jsonexport({}, {}, () => {})).pipe(output);
 }
 
 fs.readdir(folderPath, (err, files) => {
