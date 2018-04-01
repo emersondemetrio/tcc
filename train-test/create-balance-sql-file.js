d2614ad47fc29bb90d84d51e5f366e0be9714ccd
Emerson Demetrio <emer.demetrio@gmail.com>
2018-04-01 01:16:08 -0300

    TRAIN_TEST

---
+++ train-test/create-balance-sql-file.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const process = require('process');
+const resolvePath = require('path').resolve
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'mir'
+});
+
+connection.connect();
+
+const PERCENT_LIMIT = process.argv[2] ? parseInt(process.argv[2]) : 30;
+const OUTPUT_FILE = resolvePath("./results", `test_${PERCENT_LIMIT}.sql`);
+
+const createTestSetPerGenre = (limit, super_genre) => {
+	const QUERY = `SELECT * FROM music WHERE super_genre = '${super_genre}' LIMIT ${limit};`;
+
+	connection.query(QUERY, (error, results, fields) => {
+		if (error) {
+			console.log(error.message);
+		}
+		
+		let out = '';
+
+		results.forEach((result) => {
+			out += `INSERT INTO music_test SELECT * from music WHERE id = '${result.id}';\n`;
+			out += `DELETE FROM music WHERE id = '${result.id}';\n`;
+		});
+
+		fs.appendFileSync(OUTPUT_FILE, out);
+	});
+}
+
+const percent = (max) => {
+	return parseInt(PERCENT_LIMIT * (max / 100));
+}
+
+const init = (id, parsedFile, index) => {
+
+	const QUERY = `SELECT count(id) AS max, super_genre FROM music GROUP BY 2 ORDER BY 1 desc;`;
+
+	connection.query(QUERY, (error, results, fields) => {
+		if (error) {
+			console.log(error.message);
+		}
+
+		results.forEach((result) => {
+			createTestSetPerGenre(percent(result.max), result.super_genre);
+		});
+	});
+}
+
+fs.writeFile(OUTPUT_FILE, '', () => {
+	fs.appendFileSync(OUTPUT_FILE, 'TRUNCATE TABLE music_test;\n');
+	init();
+});
---
+++ train-test/create-train-file.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const process = require('process');
+const resolvePath = require('path').resolve
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'mir'
+});
+
+connection.connect();
+
+const MAX = process.argv[2] ? parseInt(process.argv[2]) : 1000;
+const OUTPUT_FILE = resolvePath("./results", `train_${MAX}.csv`);
+const HEADER = '"average_loudness";"bpm";"beats_loudness_mean";"danceability";"chords_changes_rate";"chords_number_rate";"genre"\n';
+
+const appendGenreSetToTrain = (super_genre) => {
+	const QUERY = `
+		SELECT
+			average_loudness,
+			bpm_int AS bpm,
+			beats_loudness_mean,
+			(SELECT REPLACE(danceability_d, ',', '.')) AS danceability,
+			chords_changes_rate,
+			chords_number_rate,
+			super_genre AS genre
+		FROM
+			music
+		WHERE super_genre = '${super_genre}'
+		LIMIT ${MAX};
+	`;
+
+	connection.query(QUERY, (error, results, fields) => {
+		if (error) {
+			console.log(error.message);
+		}
+
+		let out = '';
+		results.forEach((result) => {
+			out += `"${result.average_loudness}";"${result.bpm}";"${result.beats_loudness_mean}";"${result.danceability}";"${result.chords_changes_rate}";"${result.chords_number_rate}";"${result.genre}"\n`
+		});
+
+		fs.appendFileSync(OUTPUT_FILE, out);
+		console.log(super_genre);
+	});
+}
+
+const init = (id, parsedFile, index) => {
+
+	const QUERY = `SELECT DISTINCT super_genre from music;`;
+
+	connection.query(QUERY, (error, results, fields) => {
+		if (error) {
+			console.log(error.message);
+		}
+
+		results.forEach((result) => {
+			appendGenreSetToTrain(result.super_genre);
+		});
+	});
+}
+
+fs.writeFile(OUTPUT_FILE, '', () => {
+	fs.appendFileSync(OUTPUT_FILE, HEADER);
+	init();
+});
